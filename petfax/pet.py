74d5f1720c3e1b71e14cf52ebdab5481db6f9088
Mal F <malfellows13@gmail.com>
2022-09-25 11:21:05 -0500

    petfax data displayed

--- petfax/pet.py
+++ petfax/pet.py
@@ -1,6 +1,14 @@
 # CONFIG
-# import Blueprint and render_template
+# import Blueprint, render_template
 from flask import ( Blueprint, render_template)
+# needed for displaying data (at least when data is in a JSON file format)
+import json
+
+# open pets.json file, though it still won't be readable for Python yet
+# needs to be passed through json.load() for python to read it
+pets = json.load(open('pets.json'))
+# test to make sure data loads correctly!
+# print(pets)
 
 # create new instance of Blueprint
 # then need: blueprint's name, location of blueprint, and blueprint's url prefix
@@ -13,4 +21,5 @@ bp = Blueprint(
 # INDEX ROUTE
 @bp.route('/')
 def index():
-    return render_template('index.html')
+    # to get the pet data info, must pass pets as a second arg
+    return render_template('index.html', pets=pets)
